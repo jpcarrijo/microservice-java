b1bbb88406ae6ccda4a635fc2f44cd44991da426
João Paulo <carrijojp@gmail.com>
2023-02-19 10:44:07 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/example/hrpayroll/exception/handler/CustomExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.example.hrpayroll.exception.handler;
+
+import com.example.hrpayroll.exception.NotFoundException;
+import com.example.hrpayroll.exception.details.ExceptionDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@RestControllerAdvice
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class CustomExceptionHandler {
+
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<ExceptionDetails> handlerNotfound(NotFoundException notFound) {
+    ExceptionDetails notFoundDetails = ExceptionDetails.ExceptionDetailsBuilder.builder()
+        .timestamp(new Date())
+        .status(HttpStatus.NOT_FOUND.value())
+        .title("Resources Not Found!")
+        .methodService(notFound.getStackTrace()[0].getMethodName())
+        .message(notFound.getMessage())
+        .build();
+
+    return new ResponseEntity<>(notFoundDetails, HttpStatus.NOT_FOUND);
+  }
+
+
+  protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                           HttpStatus status, WebRequest request) {
+
+    Date date = new Date();
+
+    if (ex instanceof HttpClientErrorException) {
+      HttpClientErrorException exception = (HttpClientErrorException) ex;
+      ExceptionDetails exceptionDetails = ExceptionDetails.ExceptionDetailsBuilder.builder()
+          .timestamp(date)
+          .status(status.value())
+          .title("Algum dado inválido!")
+          .methodService(exception.getStackTrace()[0].getMethodName())
+          .message(exception.getMessage())
+          .build();
+      return new ResponseEntity<>(exceptionDetails, HttpStatus.BAD_REQUEST);
+    }
+    return null;
+  }
+
+}
--- hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
@@ -1,14 +1,42 @@
 package com.example.hrpayroll.services;
 
 import com.example.hrpayroll.entities.Payment;
+import com.example.hrpayroll.entities.Worker;
+import com.example.hrpayroll.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class PaymentService {
 
-  public Payment getPayment(Long workerId, Integer days) {
+  @Value("${hr-worker.host}")
+  private String host;
 
-    return new Payment("Alice", 255.0, days);
+  final RestTemplate restTemplate;
 
+  public Payment getPayment(Long workerId, Integer days) throws HttpClientErrorException {
+
+    Map<String, String> uriVariables = new HashMap<>();
+    uriVariables.put("id", "" + workerId);
+
+    Worker worker = restTemplate.getForObject(host + "/worker/{id}", Worker.class, uriVariables);
+
+    try {
+
+      assert worker != null;
+
+      return new Payment(worker.getName(), worker.getDailyIncome(), days);
+
+    } catch (HttpClientErrorException ex) {
+      throw new NotFoundException(ex.getMessage());
+    }
   }
 }
