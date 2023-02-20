989dc39ce5f8568752aa841e56847a4af58d0d21
João Paulo <carrijojp@gmail.com>
2023-02-19 21:19:15 -0300

    configuring eureka server

--- hr-eureka-server/src/main/java/com/example/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/example/hreurekaserver/HrEurekaServerApplication.java
@@ -1,13 +1,24 @@
 package com.example.hreurekaserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(HrEurekaServerApplication.class);
+
   public static void main(String[] args) {
+
+    LOGGER.info("Iniciando aplicação HrEurekaServer");
+
     SpringApplication.run(HrEurekaServerApplication.class, args);
+
+    LOGGER.info("Aplicação HrEurekaServer iniciada com sucesso!");
   }
 
 }
