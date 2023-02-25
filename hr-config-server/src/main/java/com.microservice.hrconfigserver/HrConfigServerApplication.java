fcea8a546ec391d9577e3097c0174e2a5dbe096f
jpcarrijo <carrijojp@gmail.com>
2023-02-25 18:19:34 -0300

    project hr-user created

--- hr-config-server/src/main/java/com.microservice.hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com.microservice.hrconfigserver/HrConfigServerApplication.java
@@ -1,16 +1,26 @@
 package com.microservice.hrconfigserver;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
+
+  @Value("${spring.cloud.config.server.git.username}")
+  private String username;
+
+  @Value("${spring.cloud.config.server.git.password}")
+  private String password;
 
   private static final Logger LOGGER = LoggerFactory.getLogger(HrConfigServerApplication.class);
+
 
   public static void main(String[] args) {
 
@@ -22,4 +32,12 @@ public class HrConfigServerApplication {
     LOGGER.info("Aplicação HrConfigServer iniciada com sucesso!");
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+
+    System.out.println("USERNAME = " + username);
+
+    System.out.println("PASSWORD = " + password);
+
+  }
 }
