059db7e7a8f2045bedba5ef4dfd6541b4298ee66
jpcarrijo <carrijojp@gmail.com>
2023-02-25 22:39:22 -0300

    database seeding

--- hr-user/src/main/java/com/microservice/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/microservice/hruser/HrUserApplication.java
@@ -1,15 +1,37 @@
 package com.microservice.hruser;
 
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class HrUserApplication implements CommandLineRunner {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(HrUserApplication.class);
+
+  final BCryptPasswordEncoder passwordEncoder;
 
   public static void main(String[] args) {
+
+    LOGGER.info("Iniciando aplicação HrUser");
+
     SpringApplication.run(HrUserApplication.class, args);
+
+    LOGGER.info("Aplicação HrUser iniciada com sucesso!");
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+
+//    System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
+
+  }
 }
--- hr-user/src/main/java/com/microservice/hruser/entities/User.java
+++ hr-user/src/main/java/com/microservice/hruser/entities/User.java
@@ -18,23 +18,24 @@ public class User implements Serializable {
 
   private String name;
 
-  private String password;
+  private String email;
 
+  private String password;
 
   @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(name = "tb_user_role",
-    joinColumns = @JoinColumn(name = "user_id"),
-      inverseJoinColumns = @JoinColumn(name = "role_id")
-  )
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id"))
   private Set<Role> roleSet = new HashSet<>();
 
 
   public User() {
   }
 
-  public User(Long id, String name, String password) {
+  public User(Long id, String name, String email, String password) {
     this.id = id;
     this.name = name;
+    this.email = email;
     this.password = password;
   }
 
@@ -48,6 +49,14 @@ public class User implements Serializable {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
   }
 
   public String getPassword() {
@@ -79,12 +88,14 @@ public class User implements Serializable {
   public static final class UserBuilder {
     private Long id;
     private String name;
+    private String email;
     private String password;
+    private Set<Role> roleSet;
 
     private UserBuilder() {
     }
 
-    public static UserBuilder builder() {
+    public static UserBuilder anUser() {
       return new UserBuilder();
     }
 
@@ -98,13 +109,25 @@ public class User implements Serializable {
       return this;
     }
 
+    public UserBuilder email(String email) {
+      this.email = email;
+      return this;
+    }
+
     public UserBuilder password(String password) {
       this.password = password;
       return this;
     }
 
+    public UserBuilder roleSet(Set<Role> roleSet) {
+      this.roleSet = roleSet;
+      return this;
+    }
+
     public User build() {
-      return new User(id, name, password);
+      User user = new User(id, name, email, password);
+      user.roleSet = this.roleSet;
+      return user;
     }
   }
 }
