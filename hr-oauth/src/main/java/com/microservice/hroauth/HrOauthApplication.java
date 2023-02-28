83cd88342cfb2eae2806d47ad16e5531c4be34c6
jpcarrijo <carrijojp@gmail.com>
2023-02-28 20:04:20 -0300

    configuring hr-oauth project

--- hr-oauth/src/main/java/com/microservice/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/microservice/hroauth/HrOauthApplication.java
@@ -1,13 +1,24 @@
 package com.microservice.hroauth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrOauthApplication {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(HrOauthApplication.class);
+
   public static void main(String[] args) {
+
+    LOGGER.info("Iniciando aplicação HrOauth");
+
     SpringApplication.run(HrOauthApplication.class, args);
+
+    LOGGER.info("Aplicação HrOauth iniciada com sucesso!");
   }
 
 }
